dac945ec5238f4cb9fd9acb68cdbfeb170879492
iUtkarshVats <iUtkarshVats@users.noreply.github.com>
2020-05-02 16:56:31 +0530

    changed scripts

--- sketch.js
+++ sketch.js
@@ -1,8 +1,36 @@
+const TWO_PI = 6.28318530718;
+const PI = 3.14159265359;
+
+var angle = PI / 9;
+var slider;
 
 function setup() {
-	createCanvas(400, 400);
+	slider = createSlider(0, TWO_PI, PI * 3 / 4, 0.01);
+	slider.style("width", '80%');
+	slider.style("border-color", "#515151");
+	createCanvas(window.innerWidth, window.innerHeight - 30);
+	angleMode(RADIANS);
 }
 
 function draw() {
 	background(255);
+	angle = slider.value();
+	stroke(0);
+	translate(width/2, height);
+	branch(200);
+}
+
+function branch(len) {
+	line(0, 0, 0, -len);
+	translate(0, -len);
+	if(len > 4){
+		push();
+		rotate(angle);
+		branch(len*0.67);
+		pop();
+		push();
+		rotate(-angle);
+		branch(len*0.67);
+		pop();
+	}
 }
